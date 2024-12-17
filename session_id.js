90eff1f1f1d960efe59ab032c6925e909b7fc3b9
Sfmmadu <sfmmaduwa@gmail.com>
2024-12-17 22:23:27 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&DI9ClZ7K#tKV3FBoWNdy5QVN09f7E30imq5VvztqnYPqiqOgjd38",
 };
